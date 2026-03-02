d46a5ead13de3fa1144b852cddbf740c49860198
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-02 22:58:48 +0300

    Task 6 att2

---
+++ more_functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(-10);
+	print_line(3);
+	print_line(9);
+	print_line(98);
+	return (0);
+}
