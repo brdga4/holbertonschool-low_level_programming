795bf4706da8b70265c6bed086ee4bf97797002a
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-04 23:37:39 +0300

    Task 5 att1

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string in place
+ * @s: the string
+ */
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char tmp;
+
+	while (*(s + end) != '\0')
+	{
+		end++;
+	}
+	end--;
+
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+}
