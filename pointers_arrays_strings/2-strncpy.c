7ce9c7dafe8f9c8b90b9693ba5febb2f1483187e
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-06 01:24:18 +0300

    Task 2 att1

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: bytes
+ *
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
