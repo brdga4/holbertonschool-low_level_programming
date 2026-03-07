8c71b95ed505208f58b1bfdf4e5e6fbfa604bce9
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-03-07 22:04:58 +0300

    Task 4 att1

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array of integers
+ * @a: the array
+ * @n: number of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+	char tmp;
+
+	while (start < end)
+	{
+		tmp = a[start];
+		a[start] = a[end];
+		a[end] = tmp;
+		start++;
+		end--;
+	}
+}
