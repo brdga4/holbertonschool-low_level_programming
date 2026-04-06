60dea4d0784a39a271ec89b4b1f3df757d8e8c1c
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-06 15:55:17 +0300

    Task 4 att2

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -15,31 +15,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *old_ptr;
 	unsigned int i, min_size;
 
-	/* 1. If new_size is equal to zero, and ptr is not NULL */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	/* 2. If ptr is NULL, equivalent to malloc(new_size) */
 	if (ptr == NULL)
 		return (malloc(new_size));
-
-	/* 3. If new_size == old_size, do nothing and return ptr */
 	if (new_size == old_size)
 		return (ptr);
-
-	/* 4. Allocate the new memory block */
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-
-	/* 5. Copy the contents from old block to new block */
-	/* We cast to char* to copy byte by byte */
 	old_ptr = (char *)ptr;
 
-	/* Find the minimum of old_size and new_size to avoid out-of-bounds reads */
 	if (old_size < new_size)
 		min_size = old_size;
 	else
@@ -49,9 +38,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		new_ptr[i] = old_ptr[i];
 	}
-
-	/* 6. Free the old memory block */
 	free(ptr);
-
 	return (new_ptr);
 }
