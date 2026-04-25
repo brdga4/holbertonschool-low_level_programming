997bf67ab79713936f670ed6a6fd3d366d115b61
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-25 23:29:09 +0300

    Task 3 att3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,31 +31,23 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
-
-	/* 1. Check for the correct number of arguments */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	/* 2. Open file_from for reading */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* 3. Open/Create file_to for writing with truncation and 0664 permissions */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	/* 4. Read from fd_from and write to fd_to in 1024-byte chunks */
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -65,17 +57,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
-	/* 5. Check if the read loop terminated due to an error */
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* 6. Safely close both file descriptors */
 	check_close(fd_from);
 	check_close(fd_to);
-
 	return (0);
 }
