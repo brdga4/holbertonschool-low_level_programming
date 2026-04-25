376678b47e4a827c1dd755aba16c198dd7804bfa
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-04-25 23:24:25 +0300

    task 0 and 3

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,50 +1,59 @@
 #include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - reads a text file and prints it to STDOUT
- * @filename: name of the file
- * @letters: number of letters to read
+ * read_textfile - reads a text file and prints it to the POSIX STDOUT.
+ * @filename: a pointer to the name of the file to read.
+ * @letters: the number of letters it should read and print.
  *
- * Return: number of letters printed, or 0 on failure
+ * Return: the actual number of letters it could read and print,
+ * or 0 if filename is NULL, or if open, read, or write fails.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t r, w;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
+	/* Open the file in Read-Only mode */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
 
-	r = read(fd, buffer, letters);
-	if (r == -1)
+	/* Read from the file into the buffer */
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w != r)
+	/* Write the buffer to Standard Output */
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
+	/* Clean up */
 	free(buffer);
 	close(fd);
 
-	return (r);
+	return (bytes_written);
 }
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,74 +1,72 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * main - copies content of a file to another file
- * @argc: number of arguments
- * @argv: arguments
- *
- * Return: 0 on success
+ * error_exit - Prints error messages and exits with appropriate codes
+ * @code: The exit code
+ * @message: The format string for the error message
+ * @arg: The string argument (filename)
+ * @fd: The file descriptor (used for close errors)
+ */
+void error_exit(int code, const char *message, const char *arg, int fd)
+{
+	if (code == 97 || code == 98 || code == 99)
+		dprintf(STDERR_FILENO, message, arg);
+	else if (code == 100)
+		dprintf(STDERR_FILENO, message, fd);
+
+	exit(code);
+}
+
+/**
+ * main - Copies the content of a file to another file
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ * * Return: 0 on success, or exits with codes 97, 98, 99, 100 on failure
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t r, w;
+	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
 
+	/* Check correct number of arguments */
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_exit(97, "Usage: cp %s %s\n", argv[0], 0);
 
+	/* Open file_from for reading */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		error_exit(98, "Error: Can't read from file %s\n", argv[1], 0);
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	/* Open file_to for writing, create if doesn't exist, truncate if it does */
+	/* Permissions: rw-rw-r-- (0664) */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2], 0);
+
+	/* Read from file_from and write to file_to in 1024-byte chunks */
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+			error_exit(99, "Error: Can't write to %s\n", argv[2], 0);
 	}
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w != r)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
+	/* Check if read loop exited due to an error */
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1], 0);
 
-	if (r == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-
+	/* Close file descriptors and check for errors */
 	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_from);
 
 	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_to);
 
 	return (0);
 }
